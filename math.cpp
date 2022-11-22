e09dff17de403fce220fe0c078bcd2d28dab8ac9
meowscules <79730328+meowscules@users.noreply.github.com>
2022-11-22 17:44:10 +0300

    parsing args

--- math.cpp
+++ math.cpp
@@ -2,20 +2,33 @@
 #include "Calculation.h"
 
 int main(int argc, char* argv[])
-{
-	if (strcmp(argv[1], "-h") == 0) {
-		std::cout << "usage: math plane-speed distance" << std::endl;
-		return 0;
-	}
-	if (argc < 3) {
+{	
+	switch (argc){
+	case 1:{
 		std::cout << "Missing arguments. Type math -h for help" << std::endl;
-		return 0;
+		break;
 	}
-
-	Calculation numberOne;
-	numberOne.setairplaneSpeed(atoi(argv[1]));
-	numberOne.setobsFromPlaneDistance(atoi(argv[2]));
-	std::cout << numberOne.computing() << std::endl;
+	case 2:{
+		if (strcmp(argv[1], "-h") == 0) {
+			std::cout << "usage: math plane-speed distance" << std::endl;
+		}
+		else {
+			std::cout << "Missing arguments. Type math -h for help" << std::endl;
+		}
+		break;
+	}
+	case 3:{
+		Calculation numberOne;
+		numberOne.setairplaneSpeed(atoi(argv[1]));
+		numberOne.setobsFromPlaneDistance(atoi(argv[2]));
+		std::cout << numberOne.computing() << std::endl;
+		break;
+	}
+	default: {
+		std::cout << "Type math -h for help" << std::endl;
+		break;
+	}
+	}
 
 }
 
